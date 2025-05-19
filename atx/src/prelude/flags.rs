9a4b8c22030663c4ef7da41db097ed22ae9bbe43
Chesed <potatolegend200@gmail.com>
2025-05-20 02:26:00 +0530

    whatever the fuck the last commit was [1.5/2]
    not sure if it compiles yet, just using a savepoint

--- atx/src/prelude/flags.rs
+++ atx/src/prelude/flags.rs
@@ -1,17 +1,29 @@
-use toml::Value;
-use crate::prelude::engines;
-use std::marker::PhantomData;
 //flags.rs 
+use toml::Value;
+use std::marker::PhantomData;
+use crate::prelude::notifs::*;
 
 ///determines which data should be processed or skipped.
+#[derive(Debug)]
 pub struct ParserFlags<Engine> {
     table: Value,
     engine: PhantomData<Engine>,
 }
 
 impl <Engine> ParserFlags<Engine> {
-    fn from_toml(table: Value) -> Self {
-        ParserFlags { table, PhantomData }
+    ///assumes ownership of the table provided to the flag.
+    pub fn from_toml(table: Value) -> Self {
+        ParserFlags { table, engine: PhantomData }
     }
-    fn get(field: &str) ->
+
+    ///attempts to fetch a raw &Value from the toml table.
+    pub fn get_raw(&self, field: &str) -> Success<&Value> {
+        match &self.table.get(field) {
+            Some(val) => Success::new(val),
+            None => {
+                Success::new(&Value::Boolean(false))
+                    .warn(&format!("No field detected for query {}, defaulting to false", field))
+            }
+        }
+    }
 }
--- atx/src/prelude/notifs.rs
+++ atx/src/prelude/notifs.rs
@@ -5,10 +5,16 @@
 pub enum Notif {
     Log(String),
     Warning(String),
-    Disaster(String), //well, that just happened *laugh track*
-}                     //i fucking hate forced irony in movies
-                      //yes this is a side tangent and you
-                      //will suffer for it
+}
+
+pub fn Log(string: &str) -> Notif {
+    Notif::Log(string.to_string())
+}
+pub fn Warning(string: &str) -> Notif {
+    Notif::Warning(string.to_string())
+}
+
+pub struct Disaster(pub String); 
 
 ///wraps the value inside of a container telling you
 ///what happened along the way.
@@ -17,4 +23,28 @@ pub enum Notif {
 pub struct Success<T> {
     pub val: T,
     pub notifs: Vec<Notif>,
+}
+
+impl<T> Success<T> {
+    ///default constructor: wraps T.
+    pub fn new(val: T) -> Self {
+        Success{val, notifs: vec![]}
+    }
+
+    ///consumes this struct and chains the notif vec into a new type N
+    pub fn chain<N>(self, new_value: N) -> Success<N> {
+        Success{ val: new_value, notifs: self.notifs }
+    }
+
+    ///attach the log into the vector...
+    pub fn log(mut self, log: &str) -> Self {
+        self.notifs.push(Log(log));
+        self
+    }
+
+    ///attach the warning into the vector...
+    pub fn warn(mut self, warning: &str) -> Self {
+        self.notifs.push(Warning(warning));
+        self
+    }
 }
