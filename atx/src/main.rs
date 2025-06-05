1789a55351c0be498206f1e8d96611684e32bfd8
Chesed <potatolegend200@gmail.com>
2025-06-05 10:44:47 +0530

    reset state
    thanks to my egregious overengineering, i have decided to call a reset button

--- atx/src/main.rs
+++ atx/src/main.rs
@@ -1,31 +1,14 @@
-#![allow(dead_code)]
-
 mod prelude {
-    pub mod flags;
-    pub mod notifs;
-    pub mod engines;
-    pub mod configs;
-    pub mod count;
-    pub mod context;
     pub mod loader;
-    pub mod preprocessor;
 }
-
-use crate::prelude::loader;
+use prelude::loader;
+use log::{info,error};
+use project_root::get_project_root;
 
 fn main() {
-    dotenvy::dotenv()
-        .expect("Failed to load .env file.");
-    let api = std::env::var("api")
-        .expect("No api in env.");
-    println!("{:?}", api);
-
-    match loader::load_parser_config() {
-        Ok(success) => {
-            println!("Loaded config with notifs {:?}", success.notifs);
-        },
-        Err(e) => {
-            eprintln!("Disaster occured: {:?}", e.0);
-        }
-    }
+    simple_logger::init().unwrap();
+    info!("Started logger!");
+    println!("{}",get_project_root().unwrap().display());
+    let res = loader::load_binary("raw/bsxx.dat")
+        .map_err(|e| error!("Error: {}", e));
 }
--- atx/src/prelude/loader.rs
+++ atx/src/prelude/loader.rs
@@ -1,27 +1,16 @@
-use toml::Value;
-use std::fs;
-use crate::prelude::notifs::*;
-use dotenvy::dotenv;
-use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
 
-const PARSER_CONFIG_LOAD_PATH: &str = "ParserConfig.toml";
-const REQUIRED_ENV_VARS: &[&str] = &[
-    "api", "key", "organization", "model", "language", "note_width", "batchsize",
-    "timeout", "file_threads", "threads", "width", "list_width", ""
-];
+const CHUNK_SIZE: usize = 1024;
 
-pub fn load_parser_config() -> Result<Success<Value>, Disaster> {
-    
-    //grab the toml string
-    let contents = fs::read_to_string(PARSER_CONFIG_LOAD_PATH)
-        .map_err(|err| Disaster(format!("Failed to read ParserConfig.toml: {}", err)))?;
-    
-    //turn that string into a table of toml::Value
-    let table = toml::from_str(&contents)
-        .map_err(|err| Disaster(format!("Failed to parse ParserConfig.toml: {}", err)))?;
-
-    Ok(
-        Success::new(table)
-            .log("Successfully loaded ParserConfig.toml")
-    )
+pub fn load_binary(path: &str) -> std::io::Result<()> {
+    let file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+    let mut buffer = [0u8; CHUNK_SIZE];
+    println!("Loading...");
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+        if bytes_read == 0 { break; }
+    }
+    Ok(())
 }
